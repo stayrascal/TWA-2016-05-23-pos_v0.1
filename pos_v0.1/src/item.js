4adf4bdb92d27a1165b86fc69068f57c144162e6
rascal <stayrascal@gmail.com>
2016-05-23 16:25:25 +0800

    refactor project

--- pos_v0.1/src/item.js
+++
@@ -1,6 +0,0 @@
-function Item(data){
-    this.barcode = data['barcode'];
-    this.name = data['name'];
-    this.unit = data['unit'];
-    this.price = data['price'];
-}
--- pos_v0.1/src/main.js
+++ pos_v0.1/src/main.js
@@ -1,51 +1,30 @@
 function printInventory(inputs) {
-    var info = {};
     var itemInfo = {};
-
-    for (var index in inputs) {
-        var item = new Item(inputs[index]);
-        itemInfo[inputs[index].barcode] = item;
-
-        info[inputs[index].barcode] = info[inputs[index].barcode] ? info[inputs[index].barcode] + 1 : 1;
-    }
-
     var sum = 0.0;
-    var message = '';
-    for (var index in info) {
-        var item = itemInfo[index];
-        var data = {};
-        data.name = item.name;
-        data.unit = item.unit;
-        data.price = item.price;
-        data.count = info[index];
-        data.total = data.count * data.price;
-        sum += data.total;
-        message += getMessage(data);
-    }
+
+    inputs.forEach(function(element) {
+        if (!itemInfo[element.barcode]) {
+            element['numbers'] = 1;
+            itemInfo[element.barcode] = element;
+        } else {
+            itemInfo[element.barcode]['numbers'] += 1;
+        }
+        sum += element.price;
+    });
 
     sumMessage = '总计：' + sum.toFixed(2) + '(元)\n';
-
-
-    // var expectText =
-    //         '***<没钱赚商店>购物清单***\n' +
-    //         message + 
-    //         '----------------------\n' +
-    //         sumMessage +
-    //         '**********************'.toString();
-
-    var expectText =
-        '***<没钱赚商店>购物清单***\n' +
-        // '名称：可口可乐，数量：5瓶，单价：3.00(元)，小计：15.00(元)\n' +
-        // '名称：雪碧，数量：2瓶，单价：3.00(元)，小计：6.00(元)\n' +
-        // '名称：电池，数量：1个，单价：2.00(元)，小计：2.00(元)\n' +
-        message + 
-        '----------------------\n' +
-        sumMessage +
-        '**********************';
+    var expectText = '***<没钱赚商店>购物清单***\n' + getItemMessage(itemInfo) + '----------------------\n' + sumMessage + '**********************';
     console.log(expectText);
 }
 
 function getMessage(data) {
-    return '名称：' + data.name + '，数量：' + data.count + data.unit + '，单价：' + data.price.toFixed(2) + '(元)，小计：' + data.total.toFixed(2) + '(元)\n';
+    return '名称：' + data.name + '，数量：' + data.numbers + data.unit + '，单价：' + data.price.toFixed(2) + '(元)，小计：' + (data.price * data.numbers).toFixed(2) + '(元)\n';
+}
 
+function getItemMessage(itemInfo) {
+    var itemMessage = '';
+    for (var element in itemInfo) {
+        itemMessage += getMessage(itemInfo[element]);
+    }
+    return itemMessage;
 }
